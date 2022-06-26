5f45abac2b1c56a28d3765a00a85ca15542c8905
Franklyn Vasquez <franklynvasquez24@gmail.com>
2022-06-26 01:31:31 -0400

    [ADD] Comments

--- LinkedList Easy/Rearrengments.h
+++ LinkedList Easy/Rearrengments.h
@@ -3,14 +3,17 @@
 void reverseLinkedList(DataController* list)
 {
 	list->current = list->firstInList;
-	DataType* nextToReverse = NULL;
-	DataType* previous = NULL;
+	DataType* nextToReverse = NULL; //Next one to reverse.
+	DataType* previous = NULL; //The one we JUST reversed.
 
 	while (list->current)
 	{
-		nextToReverse = list->current->next;
-		list->current->next = previous;
-		previous = list->current;
+		nextToReverse = list->current->next; //Save the next one to reverse.
+
+		list->current->next = previous; //Setting the next of { current } as the one that was reversed last loop.
+
+		previous = list->current; //Saving { current } as one we just reversed.
+
 		list->current = nextToReverse;
 	}
 	list->firstInList = previous;
